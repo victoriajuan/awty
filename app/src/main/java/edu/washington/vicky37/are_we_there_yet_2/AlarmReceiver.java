6aa71c2dd1cf8680b7df7aacea354c066b22eacb
Vicky Juan <vicky37@uw.edu>
2017-02-25 21:09:28 -0800

    SMS is not working

---
+++ app/src/main/java/edu/washington/vicky37/are_we_there_yet_2/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package edu.washington.vicky37.are_we_there_yet_2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by victoriajuan on 2/23/17.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String message = intent.getStringExtra("alarm");
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
